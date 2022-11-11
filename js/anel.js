730bbd7b2a3982c6670ef54ed9556e632dd17d9f
Guilherme Vieira <guilhermesv@gmail.com>
2022-11-11 16:13:05 -0300

    Teste com a interface Burnier

--- js/anel.js
+++ js/anel.js
@@ -1,19 +1,19 @@
 class Anel {
 
-  constructor(trapezio_qtd, trapezio_base, trapezio_topo, trapezio_altura, raio_interno, angulo_inicial){
+  constructor(quantidade, base, topo, altura, raio_interno, angulo_inicial){
     this.primeiro = true;
     this.angulo_inicial = radians(angulo_inicial);
-    this.trapezio_qtd = trapezio_qtd;
-    this.trapezio_base = trapezio_base/2; // Divido por 2 pra desenhar pelo centro
-    this.trapezio_topo = trapezio_topo/2;
-    this.trapezio_altura = trapezio_altura;
+    this.quantidade = quantidade;
+    this.base = base/2; // Divido por 2 pra desenhar pelo centro
+    this.topo = topo/2;
+    this.altura = altura;
     this.raio_interno = raio_interno;
   }
 
   desenha(c) {
     
-    for(let i = 0; i < this.trapezio_qtd; i++ ) {
-      let angulo = i * TWO_PI/this.trapezio_qtd;
+    for(let i = 0; i < this.quantidade; i++ ) {
+      let angulo = i * TWO_PI/this.quantidade;
       c.push();
       c.translate(width/2, height/2);
       c.rotate(angulo + this.angulo_inicial);
@@ -21,14 +21,15 @@ class Anel {
       c.beginShape();
       c.fill(0);
       c.noStroke();
-      c.vertex(this.trapezio_base, this.raio_interno);
-      c.vertex(-this.trapezio_base, this.raio_interno);
-      c.vertex(-this.trapezio_topo, this.raio_interno + this.trapezio_altura);
-      c.vertex(this.trapezio_topo, this.raio_interno + this.trapezio_altura);
+      c.vertex(this.base, this.raio_interno);
+      c.vertex(-this.base, this.raio_interno);
+      c.vertex(-this.topo, this.raio_interno + this.altura);
+      c.vertex(this.topo, this.raio_interno + this.altura);
       c.endShape();
 
       c.pop();
     }
   }
+
 }
 
--- js/sketch.js
+++ js/sketch.js
@@ -5,9 +5,9 @@ let pg;
 function setup() {
   createCanvas(800, 800);
   pg = createGraphics(width, height);
-  niveis = [new Anel(12, 20, 10, 100, 100, 0),
-    new Anel(12, 20, 10, 100, 200, 180/12),
-    new Anel(12, 20, 10, 100, 300, 0)
+  niveis = [new Anel(16, 20, 10, 100, 100, 0),
+            new Anel(16, 20, 10, 100, 200, 180/16),
+            new Anel(16, 20, 10, 100, 300, 0)
   ];
 }
 
@@ -19,5 +19,16 @@ function draw() {
   
   clear();
   image(pg, 0, 0, pg.width, pg.height);
+  
+}
+
+function callback(){
+  niveis.forEach(n => {
+    n.quantidade = parseInt(ui_data.quantidade);
+    n.base = ui_data.base / 2;
+    n.topo = ui_data.topo / 2;
+    n.altura = parseInt(ui_data.altura);
+    n.angulo_inicial = 180 / n.quantidade;
+  });
   
 }
